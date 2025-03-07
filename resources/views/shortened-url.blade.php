c2d48f8432aa4977955267298272880bf7750d71
TaketoUsui <take20030524@yahoo.co.jp>
2025-03-08 02:21:55 +0900

    [feat]javascriptのクオーテーション忘れ

--- resources/views/shortened-url.blade.php
+++ resources/views/shortened-url.blade.php
@@ -28,7 +28,7 @@
                     <h2 class="mb-2">短縮後のURL</h2>
                     <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">
                         {{ $shortUrl }}
-                        <button type="button" onclick="navigator.clipboard.writeText({{ $shortUrl }});" class="ml-4 inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-tight">
+                        <button type="button" onclick="navigator.clipboard.writeText('{{ $shortUrl }}');" class="ml-4 inline-block dark:bg-[#eeeeec] dark:border-[#eeeeec] dark:text-[#1C1C1A] dark:hover:bg-white dark:hover:border-white hover:bg-black hover:border-black px-5 py-1.5 bg-[#1b1b18] rounded-sm border border-black text-white text-sm leading-tight">
                             コピー
                         </button>
                     </p>
