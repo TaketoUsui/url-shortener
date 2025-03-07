bb829d628b69c2463877b3a555f6972097ee14d7
TaketoUsui <take20030524@yahoo.co.jp>
2025-03-07 16:44:19 +0900

    [feat]ルーティングの更新

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ShortenController;
 
 Route::get('/', function () {
+    return view('index');
+})->name('index');
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::post('/create-url', [ShortenController::class, 'create'])->name('create-url');
+
+Route::get('/{short_url}', [ShortenController::class, 'redirect'])->name('redirect');
