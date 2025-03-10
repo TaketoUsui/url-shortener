d7a3fcdc4d1f9d2ed812f0372895d371f27e8406
TaketoUsui <take20030524@yahoo.co.jp>
2025-03-10 15:00:33 +0900

    [feat]不要なページの削除

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,6 @@ Route::get('/', function () {
     return view('index');
 })->name('index');
 
-Route::get('/welcome', function () {
-    return view('welcome');
-});
-
 Route::post('/create-url', [ShortenController::class, 'create'])->name('create-url');
 
 Route::get('/{short_url}', [ShortenController::class, 'redirect'])->name('redirect');
